f5b30c481f75e97be9c781137c4493c801eb61b8
Vladislav <Vladislavmargovich@yandex.ru>
2024-07-25 11:03:31 +0300

    job's done.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Запишите выражение в формате: \"строка\" \"знак : +, -, *, /\" \"строка\" при + и -, число при * и /\".");
+        System.out.println("Запишите выражение в формате: \"строка\" пробел \"знак : +, -, *, /\" пробел \"строка\" при + и -, число при * и /\". Примеры: \"строка\" + \"строка\", \"строка\" * число");
         String input = sc.nextLine();
         System.out.println(Calculate.calculate(input));
     }
